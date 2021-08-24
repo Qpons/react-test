3ef7180598086292059df1d339383a53d6f44369
Calvin Wu <calvinwu0716@gmail.com>
2021-08-24 09:02:12 -0700

    Change all class comp to func arrow. Add entry field to listItems

--- src/components/listItem.js
+++ src/components/listItem.js
@@ -1,15 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style.css";
 
-class ListItem extends Component {
-  state = {};
-  render() {
-    return (
-      <div>
-        <button className="listItem">List Item Button</button>
-      </div>
-    );
-  }
-}
+const ListItem = () => {
+  return (
+    <div>
+      <button className="listItem">List Item Button</button>
+    </div>
+  );
+};
 
 export default ListItem;
--- src/components/listName.js
+++ src/components/listName.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
 import "./style.css";
 
-class ListName extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="listName">
-        <input type="text" value="Enter List Name" />
-        <br />
-        <button>Add List</button>
-      </div>
-    );
-  }
-}
+const ListName = () => {
+  return (
+    <div className="listName">
+      <input type="text" placeholder="Enter List Name" />
+      <br />
+      <button>Add List</button>
+    </div>
+  );
+};
 
 export default ListName;
