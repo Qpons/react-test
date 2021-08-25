d35008abb26fb259254522db20a39cc5515da17e
Calvin Wu <calvinwu0716@gmail.com>
2021-08-25 09:29:00 -0700

    Issue-1 Create entry and button on listItem to modify array in listItems

--- src/components/listItems.js
+++ src/components/listItems.js
@@ -3,8 +3,18 @@ import ListItem from "./listItem";
 
 const ListItems = () => {
   const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
 
-  const handleAddItem = (value) => setItems((items) => [...items, value]);
+  return (
+    <div>
+      <ListItem
+        items={items}
+        newItem={newItem}
+        setNewItem={setNewItem}
+        setItems={setItems}
+      />
+    </div>
+  );
 };
 
 export default ListItems;
